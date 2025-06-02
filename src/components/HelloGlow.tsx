85927a1e63e352ce609a114b8abf5e68564b3b0a
Musharraf Musharraf <manfromexistence13@gmail.com>
2025-06-02 06:53:06 +0000

    feat: hello > span heigth and width 200 and 5 px

--- src/components/HelloGlow.tsx
+++ src/components/HelloGlow.tsx
@@ -29,7 +29,7 @@ export function HelloGlow() {
 
                 .hello span {
                   position: relative;
-                  height: 20px;
+                  height: 200px;
                   width: 5px;
                 }
 
@@ -45,8 +45,8 @@ export function HelloGlow() {
                     
                   top: 0px;
                   bottom: 0px;
-                  left: -15px;
-                  right: -205px;
+                  left: -0px;
+                  right: -0px;
                 }
 
                 .hello span.start::after,
