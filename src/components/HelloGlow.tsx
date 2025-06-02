4af53c61d4cca9d0cc7f34d9accaa6ba58014b5b
Musharraf Musharraf <manfromexistence13@gmail.com>
2025-06-02 06:51:00 +0000

    feat: hello-glow is almost done

--- src/components/HelloGlow.tsx
+++ src/components/HelloGlow.tsx
@@ -7,7 +7,7 @@ export function HelloGlow() {
   return (
     <>
       <motion.div
-        className="hello flex flex-wrap justify-center mt-64"
+        className="hello flex flex-wrap justify-center mt-64 transition-all"
         whileHover={{ scale: 1.02 }}
         whileTap={{ scale: 0.98 }}
         transition={{ type: "spring", stiffness: 300, damping: 20 }}
@@ -33,31 +33,31 @@ export function HelloGlow() {
                   width: 5px;
                 }
 
-                .hello span::after,
-                .hello span::before {
+                .hello span::after {
                   content: "";
                   position: absolute;
                   animation: hello-glow 13s linear infinite;
                   animation-delay: calc(var(--i)*0.1s);
                   transition: all 0.5s ease-in-out; /* Corrected from 'transform' to 'transition' */
                   transform: rotate(90deg); /* Changed 'rotate' to 'transform: rotate()' */
-                  top: 0px; /* Corrected from -0px to 0px */
-                  bottom: 0px; /* Corrected from -0px to 0px */
-                  left: -105px;
-                  right: -105px;
+                  background: #48ff00;
+                  box-shadow: 0 0 5px #48ff00, 0 0 15px #48ff00, 0 0 30px #48ff00;
+                    
+                  top: 0px;
+                  bottom: 0px;
+                  left: -15px;
+                  right: -205px;
                 }
 
                 .hello span.start::after,
-                .hello span.end::after,
-                .hello span.start::before,
-                .hello span.end::before {
+                .hello span.end::after {
                   border-radius: var(--radius);
                 }
 
                 @keyframes hello-glow {
                   0% {
                     background: #48ff00;
-                    box-shadow: 0 0 5px #48ff00, 0 0 15px #48ff00, 0 0 30px #48ff00, 0 0 50px #48ff00;
+                    box-shadow: 0 0 5px #48ff00, 0 0 15px #48ff00, 0 0 30px #48ff00;
                     filter: hue-rotate(0deg);
                   }
 
@@ -71,3 +71,8 @@ export function HelloGlow() {
     </>
   );
 }
+
+// .hello span::before,
+
+//                 ,.hello span.start::before,
+//                 .hello span.end::before
